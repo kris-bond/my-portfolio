7a7998fda38a1c69db5b679fe05db61557c24250
kris-bond <21047155+kris-bond@users.noreply.github.com>
2023-01-25 19:30:26 +0000

    about me updated

--- src/pages/AboutMe/AboutMe.jsx
+++ src/pages/AboutMe/AboutMe.jsx
@@ -5,8 +5,10 @@ function AboutMe() {
   return (
     <div id="about-me" className='page'>
       <h2 className='page-header'>About Me</h2>
-      <p>I am a Full-Stack Engineer with an MSc in Computer Science from the University of Leeds.</p>
-      <p>I am proficient with Python, Java, JavaScript, HTML & CSS which I use to create tools, apps and websites.</p>
+      <div className='content-container'>
+        <p>I am a Full-Stack Engineer with an MSc in Computer Science from the University of Leeds.</p>
+        <p>I am proficient with Python, Java, JavaScript, HTML & CSS which I use to create tools, apps and websites.</p>
+      </div>
     </div>
   )
 }
