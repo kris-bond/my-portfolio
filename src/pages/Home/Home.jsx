8654c84e9efc1081735fc667c3d5a6c674fb820a
kris-bond <21047155+kris-bond@users.noreply.github.com>
2023-01-30 14:01:42 +0000

    updated about me & styling

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,35 +2,6 @@ import './Home.css'
 
 function Home() {
 
-  const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-
-  let interval = null;
-
-  document.querySelector("h1").onmouseover = event => {  
-    let iteration = 0;
-    
-    clearInterval(interval);
-    
-    interval = setInterval(() => {
-      event.target.innerText = event.target.innerText
-        .split("")
-        .map((letter, index) => {
-          if(index < iteration) {
-            return event.target.dataset.value[index];
-          }
-        
-          return letters[Math.floor(Math.random() * 26)]
-        })
-        .join("");
-      
-      if(iteration >= event.target.dataset.value.length){ 
-        clearInterval(interval);
-      }
-      
-      iteration += 1 / 3;
-    }, 35);
-  }
-
   return (
     <div id="home" className='page'>
       <h1 className="name" data-value="KRIS BOND">Kris Bond</h1>
@@ -40,5 +11,34 @@ function Home() {
     </div>
   )
 }
+
+// const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+// let interval = null;
+
+// document.querySelector("h1").onmouseover = event => {  
+//   let iteration = 0;
+  
+//   clearInterval(interval);
+  
+//   interval = setInterval(() => {
+//     event.target.innerText = event.target.innerText
+//       .split("")
+//       .map((letter, index) => {
+//         if(index < iteration) {
+//           return event.target.dataset.value[index];
+//         }
+      
+//         return letters[Math.floor(Math.random() * 26)]
+//       })
+//       .join("");
+    
+//     if(iteration >= event.target.dataset.value.length){ 
+//       clearInterval(interval);
+//     }
+    
+//     iteration += 1 / 3;
+//   }, 35);
+// }
 
 export default Home
