38b7e0f64b74cc55a7e2f9670efd97b5b6738c5b
kris-bond <21047155+kris-bond@users.noreply.github.com>
2023-02-15 20:06:38 +0000

    changed email functionality

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import AboutMe from './pages/AboutMe/AboutMe'
 import Projects from './pages/Projects/Projects'
 import Contacts from './pages/Contacts/Contacts'
 import './index.css'
+import { Toaster } from 'react-hot-toast';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
@@ -13,6 +14,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       <AboutMe/>
       <Projects/>
       <Contacts/>
+      <Toaster/>
     </div>
   </React.StrictMode>,
 )
--- src/pages/Contacts/Contacts.jsx
+++ src/pages/Contacts/Contacts.jsx
@@ -1,4 +1,5 @@
 import './Contacts.css'
+import toast from 'react-hot-toast';
 
 import {AiOutlineMail, AiFillGithub, AiFillLinkedin} from 'react-icons/ai'
 
@@ -8,12 +9,25 @@ function Contacts() {
     <div id="contacts" className='page'>
       <h2 className='page-header'>Contacts</h2>
       <div className='card-container'>
-        <a href='mailto:kris4bond@gmail.com' className='card'><AiOutlineMail className='icon'/></a>
+        <a className='card' onClick={(event) => copyClipboard()}><AiOutlineMail className='icon'/></a>
         <a href='https://github.com/kris-bond' className='card'><AiFillGithub className='icon'/></a>
         <a href='https://www.linkedin.com/in/kristofer-bond-95b790a9/' className='card'><AiFillLinkedin className='icon'/></a>
       </div>
     </div>
   )
+}
+
+//href='mailto:kris4bond@gmail.com'
+
+function copyClipboard() {
+
+  /* Copy the text inside the text field */
+  navigator.clipboard.writeText('kris4bond@gmail.com');
+
+  /* Alert the copied text */
+  toast('copied to clipboard', {
+    icon: '📋',
+  });
 }
 
 export default Contacts
